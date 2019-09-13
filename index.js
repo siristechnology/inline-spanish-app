a6808889af97e309c8f651c078a667ac7989c2e9
Suraj Shrestha <syuraj@gmail.com>
2019-09-13 18:42:54 -0400

    Add hide warning

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-import { AppRegistry } from 'react-native'
+import { AppRegistry, YellowBox } from 'react-native'
 import App from './app'
 import { name as appName } from './app.json'
 
 AppRegistry.registerComponent(appName, () => App)
+
+YellowBox.ignoreWarnings([
+	'Warning: componentWillMount is deprecated',
+	'Warning: componentWillReceiveProps is deprecated',
+	'Module RCTImageLoader requires',
+])
