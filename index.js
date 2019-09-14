3fe8354bc4e7cd88d4fb84918c2f001338b5c140
Suraj Shrestha <syuraj@gmail.com>
2019-09-14 03:36:17 -0400

    Integrate apollo graphql and fetch real data (#6)
    * WIP configured apollo graphql, ran a test query
    
    * Show data from graphql endpoint on screen
    
    * Add app screenshot in readme

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+import React from 'react'
 import { AppRegistry, YellowBox } from 'react-native'
 import App from './app'
 import { name as appName } from './app.json'
-
-AppRegistry.registerComponent(appName, () => App)
+import { ApolloProvider } from '@apollo/react-hooks'
+import GraphqlClient from './src/graphql/graphql-client'
 
 YellowBox.ignoreWarnings([
 	'Warning: componentWillMount is deprecated',
 	'Warning: componentWillReceiveProps is deprecated',
 	'Module RCTImageLoader requires',
 ])
+
+const ApolloApp = () => (
+	<ApolloProvider client={GraphqlClient}>
+		<App />
+	</ApolloProvider>
+)
+
+AppRegistry.registerComponent(appName, () => ApolloApp)
--- src/screens/cnn/cnn-screen.tsx
+++ src/screens/cnn/cnn-screen.tsx
@@ -1,56 +1,40 @@
 import React from 'react'
+import { Text } from 'react-native'
 import { NavigationScreenProps } from 'react-navigation'
-// import { Article } from '@src/core/model'
-// import { articles } from '@src/core/data/article'
+import gql from 'graphql-tag'
+import { useQuery } from '@apollo/react-hooks'
+
 import { ArticleList1 } from './articleList1.component'
 
-interface State {
-	// articles: Article[]
+export const FETCH_ARTICLES_QUERY = gql`
+	query fetchArticles {
+		fetchArticles {
+			title
+			url
+			lead_image_url
+			excerpt
+			content
+			source {
+				name
+				logoLink
+			}
+			author
+			date_published
+		}
+	}
+`
+
+export default function CnnScreen() {
+	const { loading, error, data } = useQuery(FETCH_ARTICLES_QUERY, {
+		variables: {},
+	})
+	if (loading) return <Text>Loading...</Text>
+	if (error) return <Text>Error!</Text>
+
+	return <CnnArticleList articles={data.fetchArticles} />
 }
 
-export const articles = [
-	{
-		image:
-			'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
-		title: 'How To Eat Healthy',
-		description:
-			'The easiest way to make sure your inter meal nibbling stays on track is to have...',
-		content:
-			"There's a lot of advice out there on how to eat healthy, and if we're being honest, it can sometimes feel like too much to think about. Especially when you're hungry. Remember when you were a kid and eating was as simple as open, chew, enjoy? Yes, those were simpler times. Now, knowing how to eat healthy doesn't seem quite as straightforward. Between the diet fads, gourmet trends, and a rotating roster of superfoods, eating well has gotten, well, complicated.",
-		author: {
-			firstName: 'suraj',
-			lastName: 'shrestha',
-		},
-		date: 'Today 12:35 pm',
-		tips: 10,
-		likes: 320,
-	},
-	{
-		image:
-			'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
-		title: 'How To cook',
-		description:
-			'The easiest way to make sure your inter meal nibbling stays on track is to have...',
-		content:
-			"There's a lot of advice out there on how to eat healthy, and if we're being honest, it can sometimes feel like too much to think about. Especially when you're hungry. Remember when you were a kid and eating was as simple as open, chew, enjoy? Yes, those were simpler times. Now, knowing how to eat healthy doesn't seem quite as straightforward. Between the diet fads, gourmet trends, and a rotating roster of superfoods, eating well has gotten, well, complicated.",
-		author: {
-			firstName: 'suraj',
-			lastName: 'shrestha',
-		},
-		date: 'Today 12:35 pm',
-		tips: 10,
-		likes: 320,
-	},
-]
-
-export default class CnnScreen extends React.Component<
-	NavigationScreenProps,
-	State
-> {
-	public state: State = {
-		articles: articles,
-	}
-
+class CnnArticleList extends React.Component<NavigationScreenProps> {
 	private onItemPress = article => {}
 	private onItemLikePress = article => {}
 	private onItemCommentPress = article => {}
@@ -58,7 +42,7 @@ export default class CnnScreen extends React.Component<
 	public render() {
 		return (
 			<ArticleList1
-				articles={articles}
+				articles={this.props.articles}
 				onItemPress={this.onItemPress}
 				onItemLikePress={this.onItemLikePress}
 				onItemCommentPress={this.onItemCommentPress}
