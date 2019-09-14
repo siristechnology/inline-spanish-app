8d85807434ae2504be104912c360c0b964fae0e6
Suraj Shrestha <syuraj@gmail.com>
2019-09-13 20:46:21 -0400

    dummy page with a card from ui-kitten demo app

---
+++ src/components/articles/index.ts
@@ -0,0 +1,6 @@
+export {
+	ArticleActivityBar,
+	ArticleActivityBarProps,
+} from './articleActivityBar.component'
+export { ArticleHeader, ArticleHeaderProps } from './articleHeader.component'
+export { ArticleTips, ArticleTipsProps } from './articleTips.component'
